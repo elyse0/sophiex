e7c18d9be5c9544540ae0b8bbba13518b3144a4b
Elyse <26639800+elyse0@users.noreply.github.com>
2022-12-25 03:30:08 -0600

    Extract fragment program date times and standardize use of milliseconds

--- internal/parser/hls.go
+++ internal/parser/hls.go
@@ -5,15 +5,16 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
+	"time"
 )
 
 type HlsFragment struct {
-	MediaSequence int     `json:"mediaSequence"`
-	Discontinuity int     `json:"discontinuity"`
-	Url           string  `json:"url"`
-	Duration      float64 `json:"duration"`
-	Start         float32 `json:"start"`
-	End           float32 `json:"end"`
+	MediaSequence int    `json:"mediaSequence"`
+	Discontinuity int    `json:"discontinuity"`
+	Url           string `json:"url"`
+	Duration      int64  `json:"duration"` // Milliseconds
+	Start         int64  `json:"start"`    // Unix milliseconds
+	End           int64  `json:"end"`      // Unix milliseconds
 }
 
 type HlsMediaManifest struct {
@@ -24,9 +25,19 @@ type HlsMediaManifest struct {
 func (mediaManifest HlsMediaManifest) GetFragments() ([]HlsFragment, error) {
 	var fragments []HlsFragment
 
+	var isLive = strings.Contains(mediaManifest.manifest, "#EXT-X-PLAYLIST-TYPE:VOD")
+	var programDateTime int64
+
+	// Some livestreams don't provide program date times, this way at least we can approximate it.
+	if isLive {
+		programDateTime = time.Now().UnixMilli()
+	} else {
+		programDateTime = 0
+	}
+
 	mediaSequence := 0
 	discontinuity := 0
-	var duration float64 = 0
+	var duration int64 = 0
 
 	for _, line := range strings.Split(strings.TrimSuffix(mediaManifest.manifest, "\n"), "\n") {
 		line = strings.TrimSpace(line)
@@ -42,13 +53,17 @@ func (mediaManifest HlsMediaManifest) GetFragments() ([]HlsFragment, error) {
 				fragmentUrl = urlJoin
 			}
 
+			fragmentStart := programDateTime
+			fragmentEnd := programDateTime + duration
+			programDateTime += duration
+
 			fragments = append(fragments, HlsFragment{
 				MediaSequence: mediaSequence,
 				Discontinuity: discontinuity,
 				Url:           fragmentUrl,
 				Duration:      duration,
-				Start:         0,
-				End:           0,
+				Start:         fragmentStart,
+				End:           fragmentEnd,
 			})
 
 			mediaSequence += 1
@@ -57,11 +72,17 @@ func (mediaManifest HlsMediaManifest) GetFragments() ([]HlsFragment, error) {
 		} else if strings.HasPrefix(line, "#EXTINF") {
 			re := regexp.MustCompile(`#EXTINF\s*:\s*([\d+.]+)`)
 			match := re.FindStringSubmatch(line)
-			duration, _ = strconv.ParseFloat(match[1], 64)
+			durationFloat, _ := strconv.ParseFloat(match[1], 64)
+			duration = int64(durationFloat * 1000)
 		} else if strings.HasPrefix(line, "#EXT-X-MEDIA-SEQUENCE") {
 			re := regexp.MustCompile(`#EXT-X-MEDIA-SEQUENCE\s*:\s*(\d+)`)
 			match := re.FindStringSubmatch(line)
 			mediaSequence, _ = strconv.Atoi(match[1])
+		} else if strings.HasPrefix(line, "#EXT-X-PROGRAM-DATE-TIME") {
+			re := regexp.MustCompile(`#EXT-X-PROGRAM-DATE-TIME\s*:\s*([\w-:]+)`)
+			match := re.FindStringSubmatch(line)
+			tt, _ := time.Parse(time.RFC3339, match[1])
+			programDateTime = tt.UnixMilli()
 		}
 	}
 
