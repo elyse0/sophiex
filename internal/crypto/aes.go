56ecdcdd541dd4f90389191f7de3e658573991ba
Elyse <26639800+elyse0@users.noreply.github.com>
2023-01-03 23:48:13 -0600

    Create empty bytes if provided IV is nil

--- internal/crypto/aes.go
+++ internal/crypto/aes.go
@@ -12,6 +12,9 @@ func AesDecrypt(ciphertext []byte, key []byte, iv []byte) (plaintext []byte, err
 	}
 
 	plaintext = make([]byte, len(ciphertext))
+	if iv == nil {
+		iv = make([]byte, 16)
+	}
 
 	aesCbcMode := cipher.NewCBCDecrypter(aesCipher, iv)
 	aesCbcMode.CryptBlocks(plaintext, ciphertext)
