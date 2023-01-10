c7b12a641ee2ca7d529e8e9e15fc720ee466ec54
Elyse <26639800+elyse0@users.noreply.github.com>
2023-01-10 00:05:56 -0600

    Remove unused Launch player method

--- internal/output/player.go
+++ internal/output/player.go
@@ -1,10 +1,8 @@
 package output
 
 import (
-	"fmt"
 	"io"
 	"os/exec"
-	"sync"
 )
 
 type MpvPlayer struct {
@@ -25,38 +23,6 @@ func (player *MpvPlayer) PlayFrom(input io.Reader) {
 		err := playerCommand.Run()
 		if err != nil {
 			panic(err)
-		}
-	}()
-}
-
-func (player *MpvPlayer) Launch(streams []*StreamDownloader, manager *sync.WaitGroup) {
-	var playerCommand *exec.Cmd
-	if len(streams) == 1 {
-		// playerCommand = exec.Command(player.bin, streams[0].Output.Path)
-		playerCommand = exec.Command(player.bin, "FIXME")
-
-		go func() {
-			err := playerCommand.Run()
-			if err != nil {
-				fmt.Print(err)
-			}
-		}()
-
-		manager.Done()
-		return
-	}
-
-	playerCommand = exec.Command(player.bin, "-")
-	playerStdin, _ := playerCommand.StdinPipe()
-
-	muxer := CreateMuxer()
-	// FIXME
-	muxer.WriteTo(nil, playerStdin, manager)
-
-	go func() {
-		err := playerCommand.Run()
-		if err != nil {
-			fmt.Print(err)
 		}
 	}()
 }
